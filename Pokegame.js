22c53e60c0092496cc8a351b1885af0c35116f56
ayoWhatItDo <130956867+ayoWhatItDo@users.noreply.github.com>
2023-07-24 03:29:50 -0700

    Update Pokegame.js

--- Pokegame.js
+++ Pokegame.js
@@ -25,14 +25,14 @@ function Pokegame(props) {
 
 Pokegame.defaultProps = {
   pokemon: [
-    { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-    { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-    { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-    { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
-    { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
-    { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-    { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-    { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
+    { id: 4, name: "Flamable Lizard", type: "fire", base_experience: 62 },
+    { id: 7, name: "Moist Turtle", type: "water", base_experience: 63 },
+    { id: 11, name: "BetaPod", type: "bug", base_experience: 72 },
+    { id: 12, name: "ButterflyButBigger", type: "flying", base_experience: 178 },
+    { id: 25, name: "PikachuDaGoat", type: "electric", base_experience: 112 },
+    { id: 39, name: "FuzzyPinkThing", type: "normal", base_experience: 95 },
+    { id: 94, name: "Ginger", type: "poison", base_experience: 225 },
+    { id: 133, name: "Steve", type: "normal", base_experience: 65 }
   ]
 };
 
